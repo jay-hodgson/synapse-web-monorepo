97eb7df60436bc52ab5191111cd4a59af1f1990f
jay-hodgson <jay.hodgson@sagebase.org>
2024-07-01 14:26:47 -0700

    additional progress

--- packages/synapse-react-client/src/components/SynapseHomepage/SynapseHomepageV2.tsx
+++ packages/synapse-react-client/src/components/SynapseHomepage/SynapseHomepageV2.tsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import { Box, Button, SxProps, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import {
+  Box,
+  Button,
+  SxProps,
+  Typography,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  Chip,
+} from '@mui/material'
 import SynapseFullLogo from '../../assets/icons/SynapseFullLogo'
 import { TypeAnimation } from 'react-type-animation'
+import { Search } from '../../assets/themed_icons'
+import { useOneSageURL } from '../../utils/hooks'
 
 export type SynapseHomepageV2Props = {}
+
+const onSearch = (value: string) => {
+  window.location.assign(`/Search:${encodeURIComponent(value)}`)
+}
 
 export const SynapseHomepageV2: React.FunctionComponent<
   SynapseHomepageV2Props
 > = ({}) => {
+  const popularSearches = [
+    'Alzheimer',
+    'Parkinson',
+    'Neurofibromatosis',
+    'HTAN',
+    'ukb-ppp',
+    'ROSMAP',
+    'GENIE',
+  ]
+  const LOGIN_LINK = '/LoginPlace:0'
+  const registrationLink = useOneSageURL('/register1')
+
+  const [searchValue, setSearchValue] = useState('')
+
   const navButtonSx: SxProps = {
     fontSize: '18px',
     lineHeight: '24px',
@@ -41,15 +70,19 @@ export const SynapseHomepageV2: React.FunctionComponent<
             gap: '20px',
           }}
         >
-          <Button sx={navButtonSx}>Solutions</Button>
+          <Button sx={navButtonSx}>Explore Portals</Button>
           <Button sx={navButtonSx}>Pricing</Button>
           <Button sx={{ ...navButtonSx, marginRight: '15px' }}>
             Sage Bionetworks
           </Button>
-          <Button size="large" variant="outlined">
+          <Button size="large" variant="outlined" href={LOGIN_LINK}>
             Login
           </Button>
-          <Button size="large" variant="contained">
+          <Button
+            size="large"
+            variant="contained"
+            href={registrationLink.toString()}
+          >
             Register Now
           </Button>
         </Box>
@@ -93,6 +126,74 @@ export const SynapseHomepageV2: React.FunctionComponent<
               />
             </Typography>
           </Box>
+        </Box>
+
+        {/* Search */}
+        <Box
+          sx={{
+            paddingTop: '70px',
+            maxWidth: '600px',
+            margin: 'auto',
+          }}
+        >
+          <FormControl fullWidth sx={{ m: 1 }}>
+            <OutlinedInput
+              startAdornment={
+                <InputAdornment
+                  position="start"
+                  sx={{ marginLeft: '20px', marginRight: '18px' }}
+                >
+                  <Search size={24} />
+                </InputAdornment>
+              }
+              placeholder="Search Synapse"
+              sx={{ fontSize: '24px', borderRadius: 96.6 }}
+              onSubmit={() => onSearch(searchValue)}
+              onChange={event => setSearchValue(event.target.value)}
+            />
+          </FormControl>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            gap: '10px',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: '30px',
+          }}
+        >
+          <Typography variant="body1">Popular Searches:</Typography>
+          {popularSearches.map(value => {
+            return (
+              <Chip
+                label={value}
+                onClick={() => onSearch(value)}
+                variant="outlined"
+                sx={{ borderColor: '#6BA89D' }}
+              />
+            )
+          })}
+        </Box>
+        <Box
+          sx={{
+            maxWidth: '800px',
+            margin: 'auto',
+            marginTop: '50px',
+          }}
+        >
+          <Typography
+            variant="headline2"
+            sx={{
+              textAlign: 'center',
+              fontSize: '32px',
+              lineHeight: '40px',
+              color: '#172430',
+              fontWeight: 600,
+            }}
+          >
+            Synapse is your ecosystem for responsible data sharing, innovative
+            data reuse, and collaboration.
+          </Typography>
         </Box>
       </Box>
     </Box>

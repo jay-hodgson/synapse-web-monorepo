c570ebad1a40b635c5064753733117b8b569cd39
jay-hodgson <jay.hodgson@sagebase.org>
2024-07-09 18:20:33 -0700

    pull out 2 more svgs

--- packages/synapse-react-client/src/components/SynapseHomepageV2/TrendingItem.tsx
+++ packages/synapse-react-client/src/components/SynapseHomepageV2/TrendingItem.tsx
@@ -4,7 +4,9 @@ import { calculateFriendlyFileSize } from '../../utils/functions/calculateFriend
 import { useGetEntityHeader } from '../../synapse-queries'
 import { NavigateNext } from '@mui/icons-material'
 import { BackendDestinationEnum, getEndpoint } from '../../utils/functions'
+import { ReactComponent as EgressIcon } from '../../assets/homepage/egress.svg'
 
+import { ReactComponent as UsersIcon } from '../../assets/homepage/users.svg'
 export type TrendingItemProps = {
   rank: number
   rowValues: (string | null)[]
@@ -86,41 +88,11 @@ export const TrendingItem: React.FunctionComponent<TrendingItemProps> = ({
         </Typography>
         <Typography variant="body1">{entityHeader?.name}</Typography>
         <Typography variant="body1">
-          <svg
-            width="17"
-            height="17"
-            viewBox="0 0 17 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M13.4668 4.04621C13.4668 4.99226 11.3029 5.75918 8.63347 5.75918C5.9641 5.75918 3.80014 4.99226 3.80014 4.04621M13.4668 4.04621C13.4668 3.10017 11.3029 2.33325 8.63347 2.33325C5.9641 2.33325 3.80014 3.10017 3.80014 4.04621M13.4668 4.04621V12.9537C13.4668 13.8997 11.3029 14.6666 8.63347 14.6666C5.9641 14.6666 3.80014 13.8997 3.80014 12.9537V4.04621M13.4668 8.49992C13.4668 9.42038 11.3029 10.1666 8.63347 10.1666C5.9641 10.1666 3.80014 9.42038 3.80014 8.49992"
-              stroke="#D7DEE4"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <EgressIcon />
           {friendlyEgressSize}
         </Typography>
         <Typography variant="body1">
-          <svg
-            width="17"
-            height="16"
-            viewBox="0 0 17 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.767 4.83337C10.767 6.21409 9.64771 7.33337 8.26697 7.33337C6.88629 7.33337 5.767 6.21409 5.767 4.83337C5.767 3.45266 6.88629 2.33337 8.26697 2.33337C9.64771 2.33337 10.767 3.45266 10.767 4.83337Z"
-              stroke="#D7DEE4"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M8.26599 9.33337C5.73561 9.33337 3.88863 11.0157 3.38039 13.2772C3.29425 13.6604 3.60546 14 3.9983 14H12.5337C12.9266 14 13.2378 13.6604 13.1517 13.2772C12.6434 11.0157 10.7964 9.33337 8.26599 9.33337Z"
-              stroke="#D7DEE4"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <UsersIcon />
           {friendlyUserCount}
         </Typography>
         <Box>

afca9e644734cc64c70a57943f03d51e9277dd6a
jay-hodgson <jay.hodgson@sagebase.org>
2026-04-28 09:29:13 -0700

    PSI-138: custom plots for AMP ALS, with ability to hide y axis tick labels

--- apps/portals/ampals/src/config/synapseConfigs/datasets.tsx
+++ apps/portals/ampals/src/config/synapseConfigs/datasets.tsx
@@ -2,12 +2,38 @@ import ampAlsAccessColumn from '@/components/AmpAlsAccessColumn'
 import type { CardConfiguration } from 'synapse-react-client/components/CardContainer/CardConfiguration'
 import type { LabelLinkConfig } from 'synapse-react-client/components/CardContainerLogic/CardContainerLogic'
 import type { QueryWrapperPlotNavProps } from 'synapse-react-client/components/QueryWrapperPlotNav/QueryWrapperPlotNav'
+import type { QueryWrapperSynapsePlotProps } from 'synapse-react-client/components/QueryWrapperPlotNav/QueryWrapperSynapsePlot'
 import * as SynapseConstants from 'synapse-react-client/utils/SynapseConstants'
 import { TableToGenericCardMapping } from 'synapse-react-client/components/GenericCard/TableRowGenericCard'
 import columnAliases from '../columnAliases'
 import { datasetsSql } from '../resources'
 
 const rgbIndex = 0
+
+const datasetItemCountPlotConfig: QueryWrapperSynapsePlotProps = {
+  title: 'Dataset Item Count',
+  query:
+    'SELECT name, datasetItemCount FROM syn66496326 ORDER BY datasetItemCount DESC',
+  type: 'bar',
+  horizontal: true,
+  xtitle: 'Item Count',
+  ytitle: 'Dataset',
+  showlegend: false,
+  hideYAxisTickLabels: true,
+}
+
+const datasetSizePlotConfig: QueryWrapperSynapsePlotProps = {
+  title: 'Dataset Size',
+  query:
+    'SELECT name, datasetSizeInBytes FROM syn66496326 ORDER BY datasetSizeInBytes DESC',
+  type: 'bar',
+  horizontal: true,
+  xtitle: 'Size (bytes)',
+  ytitle: 'Dataset',
+  showlegend: false,
+  hideYAxisTickLabels: true,
+}
+
 export const datasetColumnLinks: LabelLinkConfig = [
   {
     isMarkdown: false,
@@ -41,6 +67,7 @@ export const datasetQueryWrapperPlotNavProps: QueryWrapperPlotNavProps = {
   },
   isInfinite: true,
   initialLimit: 50,
+  customPlots: [datasetItemCountPlotConfig, datasetSizePlotConfig],
 }
 
 export const datasetSchema: TableToGenericCardMapping = {
